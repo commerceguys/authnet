aec153d705b06c2dab56191deb92e59919c3a8c5
Matt Glaman <nmd.matt@gmail.com>
2016-09-25 07:52:36 -0500

    Add the CommerceGuys solution ID.

--- src/DataTypes/TransactionRequest.php
+++ src/DataTypes/TransactionRequest.php
@@ -14,6 +14,7 @@ class TransactionRequest extends BaseDataType
     public function addPayment(CreditCard $creditCard)
     {
         $this->properties['payment'][$creditCard->getType()] = $creditCard->toArray();
+        $this->properties['solution']['id'] = 'A1000009';
     }
     public function addOrder(Order $order)
     {
