9a734f1acc7569c7b3ee774de35b6ea2899920f3
David Hemphill <davidlee.hemphill@gmail.com>
2017-01-27 06:05:00 -0600

    Add support for BankAccount transaction requests

--- tests/CreateTransactionRequest/AuthCaptureTest.php
+++ tests/CreateTransactionRequest/AuthCaptureTest.php
@@ -36,4 +36,36 @@ class AuthCaptureTest extends CreateTransactionRequestTestBase
     $this->assertEquals('Successful.', $response->getMessages()[0]->getText());
     $this->assertEquals('Ok', $response->getResultCode());
   }
+
+  public function testBankAccountAuthCaptureTransaction()
+  {
+    $transactionRequest = $this->createChargableBankAccountTransactionRequest(TransactionRequest::AUTH_CAPTURE);
+
+    // XML
+    $request = $this->xmlRequestFactory
+      ->createTransactionRequest()
+      ->setTransactionRequest($transactionRequest);
+    $response = $request->execute();
+
+    $this->assertTrue(isset($response->transactionResponse));
+    $this->assertEquals('I00001', $response->getMessages()[0]->getCode());
+    $this->assertEquals('Successful.', $response->getMessages()[0]->getText());
+    $this->assertEquals('Ok', $response->getResultCode());
+
+    // Use a new number, otherwise a duplicate transaction is flagged.
+    $transactionRequest = $this->createChargableBankAccountTransactionRequest(
+      TransactionRequest::AUTH_CAPTURE,
+      '0123456789'
+    );
+
+    // JSON
+    $request = $this->jsonRequestFactory
+      ->createTransactionRequest()
+      ->setTransactionRequest($transactionRequest);
+    $response = $request->execute();
+    $this->assertTrue(isset($response->transactionResponse));
+    $this->assertEquals('I00001', $response->getMessages()[0]->getCode());
+    $this->assertEquals('Successful.', $response->getMessages()[0]->getText());
+    $this->assertEquals('Ok', $response->getResultCode());
+  }
 }
--- tests/CreateTransactionRequest/CreateTransactionRequestTestBase.php
+++ tests/CreateTransactionRequest/CreateTransactionRequestTestBase.php
@@ -2,10 +2,11 @@
 
 namespace CommerceGuys\AuthNet\Tests\CreateTransactionRequest;
 
-use CommerceGuys\AuthNet\DataTypes\CreditCard;
-use CommerceGuys\AuthNet\DataTypes\Order;
-use CommerceGuys\AuthNet\DataTypes\TransactionRequest;
 use CommerceGuys\AuthNet\Tests\TestBase;
+use CommerceGuys\AuthNet\DataTypes\Order;
+use CommerceGuys\AuthNet\DataTypes\CreditCard;
+use CommerceGuys\AuthNet\DataTypes\BankAccount;
+use CommerceGuys\AuthNet\DataTypes\TransactionRequest;
 
 abstract class CreateTransactionRequestTestBase extends TestBase {
 
@@ -34,4 +35,29 @@ abstract class CreateTransactionRequestTestBase extends TestBase {
     return $transactionRequest;
   }
 
+  /**
+   * @param $type
+   * @param $accountNumber
+   * @param $routeingNumber
+   * @return \CommerceGuys\AuthNet\DataTypes\TransactionRequest
+   */
+  protected function createChargableBankAccountTransactionRequest($type, $accountNumber = '0987654321', $routingNumber = '325070760')
+  {
+    $transactionRequest = new TransactionRequest([
+      'transactionType' => $type,
+      'amount' => 5.00,
+    ]);
+    $transactionRequest->addPayment(new BankAccount([
+        'accountType' => 'checking',
+        'routingNumber' => $routingNumber,
+        'accountNumber' => $accountNumber,
+        'nameOnAccount' => 'Dwayne Johnson',
+        'bankName' => 'Bank of America'
+    ]));
+    $transactionRequest->addOrder(new Order([
+      'invoiceNumber' => 'INV-' . rand(10, 100),
+    ]));
+
+    return $transactionRequest;
+  }
 }
