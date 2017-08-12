986d9d0b801a4fcb5b79500e746f044e7b75a135
KC Erb <iamkcerb@gmail.com>
2017-08-11 18:16:32 -0600

    fixed white-space issue

--- src/UpdateCustomerPaymentProfileRequest.php
+++ src/UpdateCustomerPaymentProfileRequest.php
@@ -13,34 +13,34 @@ use CommerceGuys\AuthNet\Request\RequestInterface;
 class UpdateCustomerPaymentProfileRequest extends BaseApiRequest
 {
 
-  /**
-   * @var \CommerceGuys\AuthNet\DataTypes\PaymentProfile
-   */
-  protected $paymentProfile;
+    /**
+     * @var \CommerceGuys\AuthNet\DataTypes\PaymentProfile
+     */
+    protected $paymentProfile;
 
-  protected $customerProfileId;
+    protected $customerProfileId;
 
-  protected $validationMode = 'testMode';
+    protected $validationMode = 'testMode';
 
-  public function setPaymentProfile(PaymentProfile $profile)
-  {
-    $this->paymentProfile = $profile;
-  }
+    public function setPaymentProfile(PaymentProfile $profile)
+    {
+        $this->paymentProfile = $profile;
+    }
 
-  public function setCustomerProfileId($id)
-  {
-    $this->customerProfileId = $id;
-  }
+    public function setCustomerProfileId($id)
+    {
+        $this->customerProfileId = $id;
+    }
 
-  public function setValidationMode($validationMode)
-  {
-    $this->validationMode = $validationMode;
-  }
+    public function setValidationMode($validationMode)
+    {
+        $this->validationMode = $validationMode;
+    }
 
-  protected function attachData(RequestInterface $request)
-  {
-    $request->addData('customerProfileId', $this->customerProfileId);
-    $request->addDataType($this->paymentProfile);
-    $request->addData('validationMode', $this->validationMode);
-  }
+    protected function attachData(RequestInterface $request)
+    {
+        $request->addData('customerProfileId', $this->customerProfileId);
+        $request->addDataType($this->paymentProfile);
+        $request->addData('validationMode', $this->validationMode);
+    }
 }
